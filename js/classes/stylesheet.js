b4596ef9a6e630185419c00415ba12f696a7960d
Nicolas Gandini Lopez <nicolasgandinilopez@gmail.com>
2022-08-10 20:56:48 -0300

    operador and

--- js/classes/stylesheet.js
+++ js/classes/stylesheet.js
@@ -1,46 +1,41 @@
-const stylesheet = document.getElementById("stylesheet")
-cargarPreferenciaModo()
+const stylesheet = document.getElementById("stylesheet");
+cargarPreferenciaModo();
 
-console.log(stylesheet)
+console.log(stylesheet);
 const botonLM = document.getElementById("botonLM");
 botonLM.addEventListener("click", () => {
   lightMode();
-  console.log(stylesheet)
+  console.log(stylesheet);
 });
 
 const botonDM = document.getElementById("botonDM");
 botonDM.addEventListener("click", () => {
   darkMode();
-  console.log(stylesheet)
+  console.log(stylesheet);
 });
 
 function lightMode() {
   stylesheet.setAttribute("href", "css/estilos.css");
-  let modo = "lightMode"
-  guardarPreferenciasModo(modo)
+  let modo = "lightMode";
+  guardarPreferenciasModo(modo);
 }
 
 function darkMode() {
   stylesheet.setAttribute("href", "css/estilos2.css");
-  let modo = "darkMode"
-  guardarPreferenciasModo(modo)
+  let modo = "darkMode";
+  guardarPreferenciasModo(modo);
 }
 
-function guardarPreferenciasModo(modo){
-if(modo === "darkMode"){
-    localStorage.setItem("modo", "darkMode")
-}
-else{
-  localStorage.setItem("modo", "lightMode")
-}
+function guardarPreferenciasModo(modo) {
+  if (modo === "darkMode") {
+    localStorage.setItem("modo", "darkMode");
+  } else {
+    localStorage.setItem("modo", "lightMode");
+  }
 }
 
-function cargarPreferenciaModo(){
+function cargarPreferenciaModo() {
   let modoStorage = localStorage.getItem("modo")
-  if(modoStorage==="darkMode"){
-    darkMode()
-  }
-  else{
-    lightMode()
-  }
+  modoStorage === "darkMode" && darkMode();
+  modoStorage === "lightMode" && lightMode();
 }
